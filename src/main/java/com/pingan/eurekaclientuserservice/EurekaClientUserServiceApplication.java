9cea833d36394b9509bd0ba67995c28f8e4f3d9b
electric-wen <649356875@qq.com>
2019-08-24 16:45:26 +0800

    Initial commit

---
+++ src/main/java/com/pingan/eurekaclientuserservice/EurekaClientUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.pingan.eurekaclientuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pingan/eurekaclientuserservice/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.pingan.eurekaclientuserservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/hello")
+    public String hello() {
+        return "hello,eureka";
+    }
+}
