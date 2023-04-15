a2a5ebc4aec57181b6ec61419d7c377fa06b1ab7
周涛文 <649356875@qq.com>
2023-04-15 18:49:52 +0800

    加日志

--- src/main/java/com/pingan/eurekaclientuserservice/controller/UserController.java
+++ src/main/java/com/pingan/eurekaclientuserservice/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.pingan.eurekaclientuserservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @GetMapping("/user/hello")
     public String hello() {
+        log.info("日志打印成功：");
         return "hello,eureka";
     }
 }
